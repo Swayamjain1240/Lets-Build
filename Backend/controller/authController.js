83b0c028dd27c1164bc636b3808743cbc8a15ad2
swayamjain1240 <swayamjain1240@gmail.com>
2026-07-28 18:30:17 +0530

    middleware

--- Backend/controller/authController.js
+++ Backend/controller/authController.js
@@ -2,6 +2,11 @@ export const Login = async (req,res) => {
     try {
         const {email, password} = req.body
         
+        if(!email || !password){
+            return res.status(400).json({message:"All Field Required"})
+        }
+
+        
         
     } catch (error) {
         
