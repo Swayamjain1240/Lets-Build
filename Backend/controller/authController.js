6ae14db90e84fe9790ea4d74b5d200b992cfdc6c
swayamjain1240 <swayamjain1240@gmail.com>
2026-07-28 15:27:18 +0530

    backend auth route

---
+++ Backend/controller/authController.js
@@ -0,0 +1,9 @@
+export const Login = async (req,res) => {
+    try {
+        const {email, password} = req.body
+        
+        
+    } catch (error) {
+        
+    }
+}
--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,18 @@
 import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+import dns from "dns"
+import cookieParser from "cookie-parser"
+dns.setServers(["1.1.1.1", "8.8.8.8"])
+
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
-console.log("Hello World")
+app.use(express.json())
+app.use(cookieParser())
+
 
 app.listen(3000, ()=>{
     console.log("server is running...")
