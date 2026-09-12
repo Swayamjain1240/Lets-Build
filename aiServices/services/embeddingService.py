41d2a228e86e00257f14156ec39da9d15d41201c
swayamjain1240 <swayamjain1240@gmail.com>
2026-09-12 23:57:17 +0530

    Use FastEmbed for lightweight AI deployment

--- aiServices/services/embeddingService.py
+++ aiServices/services/embeddingService.py
@@ -1,22 +1,27 @@
-from sentence_transformers import SentenceTransformer
+from fastembed import TextEmbedding
 
-MODEL_NAME = "all-MiniLM-L6-v2"
+MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 
-model = SentenceTransformer(MODEL_NAME)
+model = TextEmbedding(
+    model_name=MODEL_NAME
+)
+
 
 def generate_embedding(text: str):
     if not text:
         return None
 
-    embedding = model.encode(text)
+    embeddings = list(
+        model.embed([text])
+    )
 
-    return embedding
+    return embeddings[0]
 
 
 def generate_embeddings(texts: list[str]):
     if not texts:
         return []
 
-    embeedings = model.encode(texts)
-
-    return embeedings
+    return list(
+        model.embed(texts)
+    )
